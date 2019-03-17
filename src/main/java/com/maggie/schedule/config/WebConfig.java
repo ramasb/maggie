3334d2a3908590bc67cc6793b0be672a9351c766
hp <balaji_ramasamy@yahoo.com>
2019-03-16 23:46:37 -0400

    Update.

--- src/main/java/com/maggie/schedule/config/WebConfig.java
+++ src/main/java/com/maggie/schedule/config/WebConfig.java
@@ -5,9 +5,15 @@ import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * The Class WebConfig.
+ */
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+	/* (non-Javadoc)
+	 * @see org.springframework.web.servlet.config.annotation.WebMvcConfigurer#configureContentNegotiation(org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer)
+	 */
 	@Override
 	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
 		configurer.favorPathExtension(false).favorParameter(true).parameterName("mediaType").ignoreAcceptHeader(true)
